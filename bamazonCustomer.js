63cd295a0e27c0e424c4cd67b7c45d37242278d5
AARON M <asmarcks@gmail.com>
2019-10-09 22:37:08 -0500

    inquirer prompt functional, connection to server established

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,31 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "jigaboo1982",
+    database: "bamazon_db"
+
+});
+
+function displayItems(){
+
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'what uh ya buyin?'
+        }
+
+    ]).then(function(input){
+
+
+
+    })
+
+};
+    displayItems();
